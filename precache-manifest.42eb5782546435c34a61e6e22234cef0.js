d8c7e36af885f178e7d286adf16900cc740da307
Anujaditya02 <anujaditya02@gmail.com>
2020-05-25 00:09:32 +0530

    Updates

---
+++ precache-manifest.42eb5782546435c34a61e6e22234cef0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638ff743c43e9242b100abe358e3b61e",
+    "url": "/ToDo-List-App-React/index.html"
+  },
+  {
+    "revision": "62dcda24b889259244e6",
+    "url": "/ToDo-List-App-React/static/css/main.c215cc80.chunk.css"
+  },
+  {
+    "revision": "36e12e41d02c32b8d909",
+    "url": "/ToDo-List-App-React/static/js/2.c7110b6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List-App-React/static/js/2.c7110b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62dcda24b889259244e6",
+    "url": "/ToDo-List-App-React/static/js/main.46d1885e.chunk.js"
+  },
+  {
+    "revision": "b717861d0077fed72113",
+    "url": "/ToDo-List-App-React/static/js/runtime-main.939929f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDo-List-App-React/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "454f3d4dc7d1011e6699c94c5fdc79ef",
+    "url": "/ToDo-List-App-React/static/media/pic1.454f3d4d.jpg"
+  }
+]);
